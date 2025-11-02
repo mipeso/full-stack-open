568ccede8852b9b41561dc44930ef8db994cc8b9
mipeso <mikasorvoja1@gmail.com>
2025-11-02 10:55:21 +0200

    Add solution to exercise kurssitiedot

---
+++ osa1/kurssitiedot/src/App.jsx
@@ -0,0 +1,50 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.part} {props.exercises}</p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part[0]} exercises={props.exercises[0]} />
+      <Part part={props.part[1]} exercises={props.exercises[1]} />
+      <Part part={props.part[2]} exercises={props.exercises[2]} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.totalExercises}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = ['Fundamentals of React', 'Using props to pass data', 'State of a component']
+  const exercises = [10, 7, 14]
+  const totalExercises = exercises[0] + exercises[1] + exercises[2]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part={parts} exercises={exercises} />
+      <Total totalExercises={totalExercises} />
+    </div>
+  )
+}
+
+export default App
